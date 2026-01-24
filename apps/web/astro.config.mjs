7e14008cae7aa8a73a6f5a53de091a1b9aee568d
Daniel Xu <hi@danielx.me>
2026-01-24 15:58:15 -0800

    make the astro site partially dynamic

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ssaucsd.org',
+  adapter: vercel(),
   integrations: [
     react(),
     sitemap(),
